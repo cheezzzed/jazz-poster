0d39488d57deaea4ea0f5d668880c03fd610924c
Johnny Sanchez <johnny062999@gmail.com>
2022-06-13 17:27:44 -0400

    almost done

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,19 @@ gsap.from('.character', { duration: 3, y: '+=8%' });
 
 gsap.from('.text', { delay: 1, duration: 2, opacity: 0, y: '-=5%' });
 
-/*
-ideas for pushing it further
-	make the stars separate
-	make the letters in JOURNEY separate, and the should start 
-	close together at first, and spread out further
-	as they come in... i think ALIEN did this...
-	the glow behind the mountain could pulse/flicker/grow and shrink
-	the mountain itself could be another layer.
-	there is no limit on how cool this could be!!!!!!!!
-*/
+// /*
+// ideas for pushing it further
+// 	make the stars separate
+// 	make the letters in JOURNEY separate, and the should start 
+// 	close together at first, and spread out further
+// 	as they come in... i think ALIEN did this...
+// 	the glow behind the mountain could pulse/flicker/grow and shrink
+// 	the mountain itself could be another layer.
+// 	there is no limit on how cool this could be!!!!!!!!
+// */
+
+
+let headSax = document.querySelector('.head-and-sax')
+headSax.addEventListener('click', function(){
+    headSax.classList.add('do-cool-shit')
+})
