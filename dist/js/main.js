494949d498ce3d8bc2150e7112664be8575e2fc6
Johnny Sanchez <johnny062999@gmail.com>
2022-06-14 15:21:38 -0400

    done!

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-console.log("Hello World from main.js!"),gsap.from(".background",{duration:2,y:"-=5%"}),gsap.from(".sand-back",{duration:3,y:"+=2%"}),gsap.from(".sand-front",{duration:3,y:"+=5%"}),gsap.from(".character",{duration:3,y:"+=8%"}),gsap.from(".text",{delay:1,duration:2,opacity:0,y:"-=5%"});let headSax=document.querySelector(".head-and-sax");headSax.addEventListener("click",(function(){headSax.classList.add("do-cool-shit")}));
+gsap.from(".poster .head-and-sax",{y:"100%",duration:2});let headSax=document.querySelector(".head-and-sax");headSax.addEventListener("click",(function(){headSax.classList.add("do-cool-shit")}));
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
-console.log('Hello World from main.js!');
+// console.log('Hello World from main.js!');
 
-gsap.from('.background', { duration: 2, y: '-=5%' });
-gsap.from('.sand-back', { duration: 3, y: '+=2%' });
-gsap.from('.sand-front', { duration: 3, y: '+=5%' });
-gsap.from('.character', { duration: 3, y: '+=8%' });
+// gsap.from('.head-and-sax', { duration: 2, y: '-=5%' });
+// gsap.from('.left-arm', { duration: 3, y: '+=2%' });
+// gsap.from('.body', { duration: 3, y: '+=5%' });
+// gsap.from('.legs', { duration: 3, y: '+=8%' });
 
-gsap.from('.text', { delay: 1, duration: 2, opacity: 0, y: '-=5%' });
+// gsap.from('.right-text', { delay: 1, duration: 2, opacity: 0, y: '-=5%' });
 
 // /*
 // ideas for pushing it further
@@ -19,7 +19,14 @@ gsap.from('.text', { delay: 1, duration: 2, opacity: 0, y: '-=5%' });
 // */
 
 
+gsap.from('.poster .head-and-sax', {
+	y: "100%",
+	duration: 2,
+})
+
 let headSax = document.querySelector('.head-and-sax')
 headSax.addEventListener('click', function(){
     headSax.classList.add('do-cool-shit')
 })
+
+
